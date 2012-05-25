b5a8c6a2aa74e443f37fda137689aa496813de01
Ka Yue Yeung <kayuey@gmail.com>
2012-05-25 23:42:59 +0800

    Add WpTestCase class.

---
+++ Test/WpTestCase.php
@@ -0,0 +1,158 @@
+<?php
+
+/*
+ * Abstract most of the unit test framework stuff, so we're not too dependent on one particular test library
+ */
+
+class WpTestCase extends PHPUnit_Framework_TestCase {
+	
+	static protected $db;
+
+	function setUp() {
+		global $wpdb;
+
+		static::$db = $wpdb;
+		static::$db->suppress_errors = false;
+		static::$db->show_errors = true;
+		static::$db->db_connect();
+
+		$this->setupErrorHandler();
+		$this->cleanUpGlobalScope();
+
+		static::startTransaction();
+	}
+
+	function tearDown() 
+	{
+		static::rollback();
+	}
+
+	private function setupErrorHandler() 
+	{
+		$handler = set_error_handler(array(&$this, 'errorHandler'));
+
+		if (is_null($handler)) {
+			restore_error_handler();
+		}
+	}
+
+	/*
+	 * Treat any error, which wasn't handled by PHPUnit as a failure
+	 */
+	function errorHandler($errno, $errstr, $errfile, $errline) 
+	{
+		// @ in front of statement
+		if (error_reporting() == 0) {
+			return;
+		}
+		
+		// notices and strict warnings are passed on to the phpunit error handler but don't trigger an exception
+		if ($errno | E_USER_ERROR | E_NOTICE | E_STRICT) {
+			PHPUnit_Util_ErrorHandler::handleError($errno, $errstr, $errfile, $errline);
+		}
+		// warnings and errors trigger an exception, which is included in the test results
+		else {
+			error_log("Testing: $errstr in $errfile on line $errline");
+
+			//TODO: we should raise custom exception here, sth like WP_PHPError
+			throw new PHPUnit_Framework_Error(
+				$errstr,
+				$errno,
+				$errfile,
+				$errline,
+				$trace
+			);
+		}
+	}
+
+	static protected function cleanUpGlobalScope() 
+	{
+		$_GET = array();
+		$_POST = array();
+		$_REQUEST = array();
+
+		if(function_exists('wp_cache_flush')) {
+			wp_cache_flush();
+		}
+	}
+
+	static protected function startTransaction() 
+	{
+		static::$db->query('SET autocommit = 0;');
+		static::$db->query('START TRANSACTION;');
+	}
+
+	static protected function rollback() 
+	{
+		static::$db->query('ROLLBACK');
+	}
+
+	function assertWPError( $actual, $message = '' ) {
+		$this->assertTrue( is_wp_error( $actual ), $message );
+	}
+
+	function assertEqualFields( $object, $fields ) {
+		foreach( $fields as $field_name => $field_value ) {
+			if ( $object->$field_name != $field_value ) {
+				$this->fail();
+			}
+		}
+	}
+
+	function assertDiscardWhitespace( $expected, $actual ) {
+		$this->assertEquals( preg_replace( '/\s*/', '', $expected ), preg_replace( '/\s*/', '', $actual ) );
+	}
+
+	function checkAtLeastPHPVersion( $version ) {
+		if ( version_compare( PHP_VERSION, $version, '<' ) ) {
+			$this->markTestSkipped();
+		}
+	}
+
+	/**
+	 * Pretend that a given URL has been requested
+	 */
+	static protected function request($url) 
+	{
+		// note: the WP and WP_Query classes like to silently fetch parameters
+		// from all over the place (globals, GET, etc), which makes it tricky
+		// to run them more than once without very carefully clearing everything
+		$_GET = $_POST = array();
+
+		foreach (array('query_string', 'id', 'postdata', 'authordata', 'day', 'currentmonth', 'page', 'pages', 'multipage', 'more', 'numpages', 'pagenow') as $v)
+			unset($GLOBALS[$v]);
+
+		$parts = parse_url($url);
+
+		if (isset($parts['scheme'])) {
+			$req = $parts['path'];
+			if (isset($parts['query'])) {
+				$req .= '?' . $parts['query'];
+				// parse the url query vars into $_GET
+				parse_str($parts['query'], $_GET);
+			}
+		}
+		else {
+			$req = $url;
+		}
+
+		$_SERVER['REQUEST_URI'] = $req;
+		unset($_SERVER['PATH_INFO']);
+
+		wp_cache_flush();
+		unset($GLOBALS['wp_query'], $GLOBALS['wp_the_query']);
+		$GLOBALS['wp_the_query'] =& new WP_Query();
+		$GLOBALS['wp_query'] =& $GLOBALS['wp_the_query'];
+		$GLOBALS['wp'] =& new WP();
+
+		// clean out globals to stop them polluting wp and wp_query
+		foreach ($GLOBALS['wp']->public_query_vars as $v) {
+			unset($GLOBALS[$v]);
+		}
+		foreach ($GLOBALS['wp']->private_query_vars as $v) {
+			unset($GLOBALS[$v]);
+		}
+
+		$GLOBALS['wp']->main(isset($parts['query']) ? $parts['query'] : '');
+	}
+}
